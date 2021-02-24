65210c051291867af0ed43ae5434844503c4dbe7
Srijan0519 <srijan16k@gmail.com>
2021-02-25 02:38:05 +0530

    sideways collision fixed

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void collision(Collision col)
+	{
+      if(col.gameObject.name == "Player")
+	  {
+		  Debug.Log("turn collision");
+	  }
+	}
+}
